23c1d341f1148d5f1396943ccb34f98f980ca59d
Arda-Kaplan <aykan.barut@me.com>
2026-07-02 17:01:55 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebas
 import { getFirestore, doc, getDoc, setDoc, onSnapshot, runTransaction, collection, addDoc, serverTimestamp, getDocs } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "DEINE_API_KEY",
+  apiKey: "AIzaSyCzbVs6AHO60h8enA5Y80tL7RqWqsBWn6g",
   authDomain: "la-casa-del-nonno.firebaseapp.com",
   projectId: "la-casa-del-nonno",
   storageBucket: "la-casa-del-nonno.firebasestorage.app",
   messagingSenderId: "751184717495",
-  appId: "DEINE_APP_ID"
+  appId: "1:751184717495:web:3452ad5e589591864fcf32"
 };
 
 export const app = initializeApp(firebaseConfig);
